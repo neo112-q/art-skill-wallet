7c466728000db5fed4b0f81c24668c515e8842a3
Neo <phonlawato55@gmail.com>
2026-06-03 06:04:53 +0700

    fix bug fontend

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -24,7 +24,7 @@ type User struct {
 }
 
 type RegisterRequest struct {
-	Username string `json:"username" binding:"required,min=3,max=30"`
+	Username string `json:"username" binding:"required,min=3,max=12"`
 	Email    string `json:"email"    binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
 }
